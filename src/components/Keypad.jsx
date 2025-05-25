e2140d8fb8de655e752d25a48ddafd725f8adc24
La Glep Lune <99890584+WiCald@users.noreply.github.com>
2025-05-25 16:40:14 -0600

    Update Keypad.jsx

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -9,32 +9,32 @@ export default function Keypad({
     <div className="keypad">
       {/* Fila 1 */}
       <Button onClick={onClear}   label="C"   className="func"/>
-      <Button onClick={onSign}    label="+/−" className="func"/>
+      <Button onClick={onSign}    label="+/-" className="func"/>
       <Button onClick={onPercent} label="%"   className="func"/>
-      <Button onClick={onOperator} label="÷"  className="op"/>
+      <Button onClick={onOperator} label="÷"   className="op"/>
 
       {/* Fila 2 */}
       <Button onClick={onDigit} label="7"/>
       <Button onClick={onDigit} label="8"/>
       <Button onClick={onDigit} label="9"/>
-      <Button onClick={onOperator} label="×" className="op"/>
+      <Button onClick={onOperator} label="×"   className="op"/>
 
       {/* Fila 3 */}
       <Button onClick={onDigit} label="4"/>
       <Button onClick={onDigit} label="5"/>
       <Button onClick={onDigit} label="6"/>
-      <Button onClick={onOperator} label="−" className="op"/>
+      <Button onClick={onOperator} label="-"   className="op"/>
 
       {/* Fila 4 */}
       <Button onClick={onDigit} label="1"/>
       <Button onClick={onDigit} label="2"/>
       <Button onClick={onDigit} label="3"/>
-      <Button onClick={onOperator} label="+" className="op"/>
+      <Button onClick={onOperator} label="+"   className="op"/>
 
       {/* Fila 5 */}
       <Button onClick={onDigit} label="0"/>
       <Button onClick={onDot}   label="."/>
-      <Button onClick={onEqual} label="=" className="op"/>
+      <Button onClick={onEqual} label="="   className="op"/>
       {/* celda vacía */}
     </div>
   )
